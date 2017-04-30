58754d59920499f16622c5e90793b243a478cd7a
gab1306 <gabmimouni@gmail.com>
2017-04-30 09:55:05 +0300

    deploy live

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.listen(process.env.PORT || '8080');
